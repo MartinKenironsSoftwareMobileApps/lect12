925e14deb380bc60a19e7eb9714f54320e2be983
unknown <martin.kenirons@gmit.ie>
2024-04-24 12:35:08 +0000

    lect 11/12

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {alarm} from 'ionicons/icons';
+import { Storage } from '@ionic/storage-angular';
+import { Router } from '@angular/router';
+
+import { Toast } from '@capacitor/toast';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonButton, FormsModule, IonToolbar, IonIcon, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  name:string = "Martin";
+  constructor(private storage:Storage, private router:Router) {
+    addIcons({alarm});
+  }
+
+ async OnButtonClick(){
+    await this.storage.create();
+    this.name = await this.storage.set('name', this.name);
+  }
+
+  async showToast(){
+    // Show the splash for two seconds and then automatically hide it:
+await SplashScreen.show({
+  showDuration: 2000,
+  autoHide: true,
+});
+    // await Toast.show({
+    //   text: 'Hello!',
+    // });
+
+  }
 }
